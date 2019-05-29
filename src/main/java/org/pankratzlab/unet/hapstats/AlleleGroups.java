e447b7afaef6b234f2d66057d93ecb139c5b2a56
krclarke <krclarke@D20190476.ad.ahc.umn.edu>
2019-05-29 11:12:04 -0500

    Format pom and AlleleGroups
    Changed XML to not auto format and removed line breaks that had been
    added. Switched AlleleGroups back to multiple single lines of comments.

--- src/main/java/org/pankratzlab/unet/hapstats/AlleleGroups.java
+++ src/main/java/org/pankratzlab/unet/hapstats/AlleleGroups.java
@@ -38,145 +38,151 @@ import com.google.common.collect.ImmutableMap.Builder;
 
 /**
  * Static utility class for looking up associated allele groupings
- * (http://hla.alleles.org/alleles/g_groups.html, http://hla.alleles.org/alleles/p_groups.html)
+ * (http://hla.alleles.org/alleles/g_groups.html,
+ * http://hla.alleles.org/alleles/p_groups.html)
  */
 public final class AlleleGroups {
 
-  private static final String EQUIV_DELIM = "/";
-  private static final String COMMENT_FLAG = "#";
-  private static final String ENTRY_DELIM = ";";
+	private static final String EQUIV_DELIM = "/";
+	private static final String COMMENT_FLAG = "#";
+	private static final String ENTRY_DELIM = ";";
 
-  private AlleleGroups() {}
+	private AlleleGroups() {
+	}
 
-  private static final String P_GROUP_PATH = "/hla_nom_p.txt";
-  private static final String G_GROUP_PATH = "/hla_nom_g.txt";
-  private static final ImmutableMap<HLAType, HLAType> P_GROUP;
-  private static final ImmutableMap<HLAType, HLAType> G_GROUP;
+	private static final String P_GROUP_PATH = "/hla_nom_p.txt";
+	private static final String G_GROUP_PATH = "/hla_nom_g.txt";
+	private static final ImmutableMap<HLAType, HLAType> P_GROUP;
+	private static final ImmutableMap<HLAType, HLAType> G_GROUP;
 
-  static {
-    P_GROUP = buildGroup(P_GROUP_PATH);
-    G_GROUP = buildGroup(G_GROUP_PATH);
-  }
+	static {
+		P_GROUP = buildGroup(P_GROUP_PATH);
+		G_GROUP = buildGroup(G_GROUP_PATH);
+	}
 
-  private static ImmutableMap<HLAType, HLAType> buildGroup(String pathToGroupFile) {
-    Builder<HLAType, HLAType> builder = ImmutableMap.builder();
-    Map<HLAType, HLAType> typeMap = new HashMap<>();
-    try (BufferedReader groupFileReader =
-        new BufferedReader(
-            new InputStreamReader(AlleleGroups.class.getResourceAsStream(pathToGroupFile)))) {
-      String line;
-      /*
-       * Each line is rather a single allele or a list of alleles followed by the group those
-       * alleles belong to Delimiter is ; First element is locus If no grouping, second element is
-       * specificity If grouping, second element is '/'-delimited list of equivalent specificities
-       * If grouping, third element is the group allele specificity
-       */
+	private static ImmutableMap<HLAType, HLAType> buildGroup(String pathToGroupFile) {
+		Builder<HLAType, HLAType> builder = ImmutableMap.builder();
+		Map<HLAType, HLAType> typeMap = new HashMap<>();
+		try (BufferedReader groupFileReader = new BufferedReader(
+				new InputStreamReader(AlleleGroups.class.getResourceAsStream(pathToGroupFile)))) {
+			String line;
+
+      // Each line is rather a single allele or a list of alleles followed by the group those alleles belong to
+      // Delimiter is ;
+      // First element is locus
+      // If no grouping, second element is specificity
+      // If grouping, second element is '/'-delimited list of equivalent specificities
+      // If grouping, third element is the group allele specificity
+
       while ((line = groupFileReader.readLine()) != null) {
-        if (line.startsWith(COMMENT_FLAG)) {
-          continue;
-        }
+				if (line.startsWith(COMMENT_FLAG)) {
+					continue;
+				}
 
-        String[] entries = line.split(ENTRY_DELIM);
-        if (entries.length == 2) {
-          continue;
-        }
-        if (entries.length != 3) {
-          throw new IllegalStateException("Unrecognized allele group line: " + line);
-        }
+				String[] entries = line.split(ENTRY_DELIM);
+				if (entries.length == 2) {
+					continue;
+				}
+				if (entries.length != 3) {
+					throw new IllegalStateException("Unrecognized allele group line: " + line);
+				}
 
-        String locus = entries[0].replace("*", "");
-        try {
-          HLALocus.valueOf(locus);
-        } catch (IllegalArgumentException e) {
-          System.err.println("AlleleGroups; not a recognized locus: " + locus);
-          continue;
-        }
+				String locus = entries[0].replace("*", "");
+				try {
+					HLALocus.valueOf(locus);
+				} catch (IllegalArgumentException e) {
+					System.err.println("AlleleGroups; not a recognized locus: " + locus);
+					continue;
+				}
 
-        final HLAType rootAllele = getAllele(locus, entries[2]);
+				final HLAType rootAllele = getAllele(locus, entries[2]);
 
-        Set<HLAType> equivs = new HashSet<>();
+				Set<HLAType> equivs = new HashSet<>();
 
-        for (String spec : entries[1].split(EQUIV_DELIM)) {
-          equivs.add(getAllele(locus, spec));
-        }
+				for (String spec : entries[1].split(EQUIV_DELIM)) {
+					equivs.add(getAllele(locus, spec));
+				}
 
-        for (HLAType equivAllele : equivs) {
-          builder.put(equivAllele, rootAllele);
-          typeMap.put(equivAllele, rootAllele);
-        }
-      }
-    } catch (Exception e) {
-      System.err.println("Failed to read allele group file: " + pathToGroupFile);
-      e.printStackTrace();
-      throw new IllegalStateException("Failed to read allele group file: " + pathToGroupFile);
-    }
+				for (HLAType equivAllele : equivs) {
+					builder.put(equivAllele, rootAllele);
+					typeMap.put(equivAllele, rootAllele);
+				}
+			}
+		} catch (Exception e) {
+			System.err.println("Failed to read allele group file: " + pathToGroupFile);
+			e.printStackTrace();
+			throw new IllegalStateException("Failed to read allele group file: " + pathToGroupFile);
+		}
 
-    return builder.build();
-  }
+		return builder.build();
+	}
 
-  /**
-   * Helper method to convert a locus + specificity string from a group file to an {@link HLAType}
-   */
-  private static HLAType getAllele(String locus, String specificity) {
-    String alleleString = locus + "*" + specificity.replaceAll("[a-mo-zA-MO-Z]", "");
-    if (alleleString.toLowerCase().endsWith("n")) {
-      return NullType.valueOf(alleleString);
-    }
-    return HLAType.valueOf(alleleString);
-  }
+	/**
+	 * Helper method to convert a locus + specificity string from a group file to an
+	 * {@link HLAType}
+	 */
+	private static HLAType getAllele(String locus, String specificity) {
+		String alleleString = locus + "*" + specificity.replaceAll("[a-mo-zA-MO-Z]", "");
+		if (alleleString.toLowerCase().endsWith("n")) {
+			return NullType.valueOf(alleleString);
+		}
+		return HLAType.valueOf(alleleString);
+	}
 
-  /**
-   * Accepts allele strings ending in n, g or p and decodes them to the appropriate g or p group
-   * 
-   * @param alleleString representation of an allele
-   * @return the g or p group allele if this allele is a member. Otherwise return allele
-   */
-  public static HLAType getGroupAllele(String alleleString) {
-    HLAType baseType = HLAType.valueOf(alleleString);
+	/**
+	 * Accepts allele strings ending in n, g or p and decodes them to the
+	 * appropriate g or p group
+	 * 
+	 * @param alleleString representation of an allele
+	 * @return the g or p group allele if this allele is a member. Otherwise return
+	 *         allele
+	 */
+	public static HLAType getGroupAllele(String alleleString) {
+		HLAType baseType = HLAType.valueOf(alleleString);
 
-    alleleString = alleleString.toLowerCase();
+		alleleString = alleleString.toLowerCase();
 
-    if (alleleString.endsWith("n")) {
-      return getUnknownGroupEquiv(NullType.valueOf(alleleString));
-    } else if (alleleString.endsWith("g")) {
-      return getGGroup(baseType);
-    } else if (alleleString.endsWith("p")) {
-      return getPGroup(baseType);
-    }
+		if (alleleString.endsWith("n")) {
+			return getUnknownGroupEquiv(NullType.valueOf(alleleString));
+		} else if (alleleString.endsWith("g")) {
+			return getGGroup(baseType);
+		} else if (alleleString.endsWith("p")) {
+			return getPGroup(baseType);
+		}
 
-    return baseType;
-  }
+		return baseType;
+	}
 
-  private static HLAType getUnknownGroupEquiv(NullType unknown) {
-    // Checking if unknown allele is in g group
-    HLAType equiv = G_GROUP.get(unknown);
-    // If null unknown is not in g group and p group should be checked
-    if (Objects.isNull(equiv)) {
-      equiv = P_GROUP.get(unknown);
-    }
-    // If equiv is still null then neither g group or p group
-    return Objects.isNull(equiv) ? unknown : equiv;
-  }
+	private static HLAType getUnknownGroupEquiv(NullType unknown) {
+		// Checking if unknown allele is in g group
+		HLAType equiv = G_GROUP.get(unknown);
+		// If null unknown is not in g group and p group should be checked
+		if (Objects.isNull(equiv)) {
+			equiv = P_GROUP.get(unknown);
+		}
+		// If equiv is still null then neither g group or p group
+		return Objects.isNull(equiv) ? unknown : equiv;
+	}
 
-  /**
-   * @param allele key value to search G_Group for
-   * @return
-   */
-  public static HLAType getGGroup(HLAType allele) {
-    return getGroupEquiv(G_GROUP, allele);
-  }
+	/**
+	 * @param allele key value to search G_Group for
+	 * @return
+	 */
+	public static HLAType getGGroup(HLAType allele) {
+		return getGroupEquiv(G_GROUP, allele);
+	}
 
-  /**
-   * @param allele
-   * @return
-   */
-  public static HLAType getPGroup(HLAType allele) {
-    return getGroupEquiv(P_GROUP, allele);
-  }
+	/**
+	 * @param allele
+	 * @return
+	 */
+	public static HLAType getPGroup(HLAType allele) {
+		return getGroupEquiv(P_GROUP, allele);
+	}
 
-  /** Helper method to look up an allele in a group map */
-  private static HLAType getGroupEquiv(ImmutableMap<HLAType, HLAType> groupMap, HLAType allele) {
-    HLAType equiv = groupMap.get(allele);
-    return Objects.isNull(equiv) ? allele : equiv;
-  }
+	/** Helper method to look up an allele in a group map */
+	private static HLAType getGroupEquiv(ImmutableMap<HLAType, HLAType> groupMap, HLAType allele) {
+		HLAType equiv = groupMap.get(allele);
+		return Objects.isNull(equiv) ? allele : equiv;
+	}
 }
